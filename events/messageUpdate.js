ddce459b304d3f047a32efb945463e30481aeaf0
Carbonax <gizzib0129@gmail.com>
2023-03-06 16:41:15 +0000

    no spam pls

--- events/messageUpdate.js
+++ events/messageUpdate.js
@@ -5,13 +5,13 @@ module.exports = {
 	once: false,
 	async execute(oldMessage, newMessage, client) {
 		if (!oldMessage.channel.name.endsWith('waiting_queue')) return;
-		const messages = await oldMessage.channel.messages.fetch({ limit: 2 });
+		const messages = await oldMessage.channel.messages.fetch().then(queue_messages => queue_messages.array().slice(-2));
 		if (!messages) return;
 		const embeds = messages.reduce((acc, msg) => msg.embeds.length && acc.concat(msg.embeds[0]), []);
 		if (!embeds.length) return;
 		const sot_leaving = oldMessage.guild.channels.cache.find(channel => channel.name == client.config.Mentions.channels.sot_leaving);
 		if (!sot_leaving) return;
-		let queue_message = await sot_leaving.messages.fetch({ limit: 1 }).then(message => console.log(message));
+		let queue_message = await sot_leaving.messages.fetch().then(leaving_messages => leaving_messages.first());
 		if (!queue_message) queue_message = await sot_leaving.send({ embeds: embeds });
 		return queue_message.edit({ embeds: embeds });
 	},
