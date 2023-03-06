de588f870bd22942e554b6e19a13b6846a1d9cdb
Carbonax <gizzib0129@gmail.com>
2023-03-06 19:05:42 +0000

    remove \u200B

--- events/messageUpdate.js
+++ events/messageUpdate.js
@@ -24,7 +24,7 @@ module.exports = {
 
 		outEmbed.data.fields.forEach(field => {
 			const split = field.value.split('\n');
-			const filtered = split.filter(line => !role_member_ids.some(id => line.includes(id))).filter(line => line);
+			const filtered = split.filter(line => !role_member_ids.some(id => line.includes(id))).filter(line => line != '' && line != '\u200B');
 			if (filtered.length == 0) return field.value = 'Empty';
 			field.value = filtered.map((line, index) => `\`${index + 1}\`${line}`).join('\n');
 		});
