e839bd7b875ec421710ffe1be34cf2ae34533327
Carbonax <gizzib0129@gmail.com>
2023-03-06 16:20:16 +0000

    queue in leaving

---
+++ events/messageUpdate.js
@@ -0,0 +1,17 @@
+const { Events } = require('discord.js');
+
+module.exports = {
+	name: Events.MessageUpdate,
+	once: false,
+	async execute(oldMessage, newMessage, client) {
+		if (!oldMessage.channel.name.endsWith('waiting_queue')) return;
+		const messages = await oldMessage.channel.messages.fetch({ limit: 2 });
+		if (!messages) return;
+		const embeds = messages.reduce((acc, msg) => acc.concat(msg.embeds[0]), []);
+		const sot_leaving = oldMessage.guild.channels.find(channel => channel.name == client.config.Mentions.channels.sot_leaving);
+		if (!sot_leaving) return;
+		const queue_message = await sot_leaving.messages.fetch({ limit: 1 }).then(queue_messages => queue_messages.first()).catch(() => null);
+		if (!queue_message) return sot_leaving.send({ embeds: embeds });
+		queue_message.edit({ embeds: embeds }).catch(() => null);
+	},
+};
