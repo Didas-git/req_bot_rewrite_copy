49388cb8491ac1bc0ed4b7ecd2dca5d39ac22a94
Carbonax <gizzib0129@gmail.com>
2023-03-06 16:33:34 +0000

    should work now?

--- events/messageUpdate.js
+++ events/messageUpdate.js
@@ -9,11 +9,10 @@ module.exports = {
 		if (!messages) return;
 		const embeds = messages.reduce((acc, msg) => msg.embeds.length && acc.concat(msg.embeds[0]), []);
 		if (!embeds.length) return;
-		let sot_leaving = oldMessage.guild.channels.cache.find(channel => channel.name == client.config.Mentions.channels.sot_leaving);
-		if (!sot_leaving) sot_leaving = await oldMessage.guild.channels.fetch().then(channels => channels.find(channel => channel.name == client.config.Mentions.channels.sot_leaving)).catch(() => null);
+		const sot_leaving = oldMessage.guild.channels.cache.find(channel => channel.name == client.config.Mentions.channels.sot_leaving);
 		if (!sot_leaving) return;
-		const queue_message = await sot_leaving.messages.fetch({ limit: 1 }).then(queue_messages => queue_messages.first()).catch(() => null);
-		if (!queue_message) return sot_leaving.send({ embeds: embeds });
-		queue_message.edit({ embeds: embeds }).catch(() => null);
+		let queue_message = await sot_leaving.messages.fetch({ limit: 1 }).then(message => console.log(message));
+		if (!queue_message) queue_message = await sot_leaving.send({ embeds: embeds });
+		return queue_message.edit({ embeds: embeds });
 	},
 };
