afc622fcca31676a0c08ca7e4b32a5f4b375d07d
Carbonax <gizzib0129@gmail.com>
2023-03-06 18:51:04 +0000

    check if embed exists before continuing

--- events/messageCreate.js
+++ events/messageCreate.js
@@ -177,7 +177,7 @@ async function updatePromptColours(leaving_ship, sot_leaving, options) {
 	const all_prompt_messages = await sot_leaving.messages.fetch().catch(e => console.error(e));
 	const filtered_prompt_messages = all_prompt_messages.filter(message => message.id != all_prompt_messages.last().id);
 
-	const prompt_messages = filtered_prompt_messages.filter(message => message.embeds.length > 0 && message.embeds[0].description.includes(leaving_ship.id));
+	const prompt_messages = filtered_prompt_messages.filter(message => message.embeds.length > 0 && message.embeds[0].description && message.embeds[0].description.includes(leaving_ship.id));
 	if (options?.exclude && prompt_messages.filter(message => message.id != options.exclude.id).size == 0) return;
 
 	if (!prompt_messages) return;
