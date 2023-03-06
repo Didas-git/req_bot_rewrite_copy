7bfd9f4b8cc40a1db8f70812be52bee08062a232
Carbonax <gizzib0129@gmail.com>
2023-03-06 17:07:51 +0000

    remove capacity

--- events/messageUpdate.js
+++ events/messageUpdate.js
@@ -1,4 +1,4 @@
-const { Events } = require('discord.js');
+const { Events, EmbedBuilder } = require('discord.js');
 
 module.exports = {
 	name: Events.MessageUpdate,
@@ -8,13 +8,22 @@ module.exports = {
 		const messages = await oldMessage.channel.messages.fetch().then(queue_messages => queue_messages.last(2));
 		if (!messages) return;
 		const embeds = messages.reduce((acc, msg) => msg.embeds.length && acc.concat(msg.embeds[0]), []).reverse();
-		embeds.forEach(embed => embed.data.description = '');
-		console.log(embeds[0].fields);
+		embeds.forEach(embed => {
+			embed.data.description = '';
+			embed.data.fields[0].name = embed.data.title;
+		});
+
+		const outEmbed = new EmbedBuilder()
+			.setTitle('Queue')
+			.setColor('e7c200');
+
+		outEmbed.data.fields = [...embeds[0].data.fields, ...embeds[1].data.fields];
+
 		if (!embeds.length) return;
 		const sot_leaving = oldMessage.guild.channels.cache.find(channel => channel.name == client.config.Mentions.channels.sot_leaving);
 		if (!sot_leaving) return;
 		let queue_message = await sot_leaving.messages.fetch().then(leaving_messages => leaving_messages.last());
-		if (!queue_message) queue_message = await sot_leaving.send({ embeds: embeds });
-		return queue_message.edit({ embeds: embeds });
+		if (!queue_message) queue_message = await sot_leaving.send({ embeds: [outEmbed] });
+		return queue_message.edit({ embeds: [outEmbed] });
 	},
 };
