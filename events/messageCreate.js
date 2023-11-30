0c0416f93621d61c091363563b8c7298c166c7df
g020276j <g020276j@student.staffs.ac.uk>
2023-11-30 18:39:06 +0000

    add invalid user message

--- events/messageCreate.js
+++ events/messageCreate.js
@@ -70,6 +70,7 @@ module.exports = {
 			console.log(args[0])
 			console.log(args[0].match(/\d+/i))
 			const whereis_user_id = args[0].match(/\d+/i);
+			if (!whereis_user_id) return message.reply('Invalid user');
 			const whereis_user = await message.guild.members.fetch(whereis_user_id).catch(() => null);
 			const user_channel = whereis_user.voice.channel;
 			if(!user_channel) return message.reply('User is not in a voice channel');
