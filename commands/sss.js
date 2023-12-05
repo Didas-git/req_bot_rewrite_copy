c895640bfd1855469794f6c8ae5e6545c9b20f6b
g020276j <g020276j@student.staffs.ac.uk>
2023-12-05 22:20:08 +0000

    try this

--- commands/sss.js
+++ commands/sss.js
@@ -62,7 +62,8 @@ module.exports = {
 
 	async permission(interaction, client) {
 		redis = client.redis;
-		redis.exists('state:alliance_locked').then(console.log);
+		const exists = await redis.exists('state:alliance_locked').catch(e => console.error(e));
+		console.log(exists);
 		return;
 		interaction.server_locked = await redis.get('state:alliance_locked').then(returnedState => Number(returnedState)) ?? true;
 		console.log(interaction.server_locked);
