b797973c0b4828ec4d9003b7d272d67b92391448
g020276j <g020276j@student.staffs.ac.uk>
2023-12-06 00:36:34 +0000

    Revert "hello? anyone home?"
    This reverts commit 30dcfc9db07b94e5ca09924be30f274364d7e957.

--- commands/sss.js
+++ commands/sss.js
@@ -71,7 +71,6 @@ module.exports = {
 		console.log(exists);
 		console.log(`\n\n${await redis.keys('*').then(keys => keys.map(key => `'${key}' - ${key == 'state:alliance_locked'}`).join('\n'))}`);
 		console.log(await redis.GET('state:alliance_locked'));
-		console.log(await redis.EXISTS('state:alliance_locked'));
 		return;
 		interaction.server_locked = await redis.get('state:alliance_locked').then(returnedState => Number(returnedState)) ?? true;
 		console.log(interaction.server_locked);
